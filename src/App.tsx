3cf766d990dc214feb5b3f3b1c471ddafcc4143c
sayel <mdsayel111@gmail.com>
2024-08-29 03:41:12 +0600

    add top scrolling button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { FloatButton } from "antd"
+import { FaArrowUp } from "react-icons/fa"
 import { Outlet } from "react-router-dom"
 import Footer from "./components/app/footer/Footer"
 import Navbar from "./components/app/navbar/Navbar"
-import ThemeProvider from "./themeProvider/ThemeProvider"
 
 function App() {
+
+  // scroll top handler
+  const scrollHandler = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 
   return (
     <>
@@ -16,6 +25,14 @@ function App() {
         <Outlet />
 
       </div>
+
+      <FloatButton
+        onClick={scrollHandler}
+        shape="square"
+        type="primary"
+        style={{ insetInlineEnd: 24 }}
+        icon={<FaArrowUp />}
+      />
 
       {/* footer */}
       <Footer />
