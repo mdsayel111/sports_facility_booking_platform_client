a73b5d471f9bf1dc247d8d2983ed28c5c2ed2a6a
sayel <mdsayel111@gmail.com>
2024-08-29 04:15:28 +0600

    add search and filter funtionality

--- src/pages/facility/Facility.tsx
+++ src/pages/facility/Facility.tsx
@@ -3,6 +3,9 @@ import { useGetAllFacilityQuery } from "../../redux/api/facility-api";
 import FacilityCard from "../../components/shared/facility-card/FacilityCard";
 import { TFacilityData } from "../../type";
 import Title from "../../components/shared/title/Title";
+import Search, { SearchProps } from "antd/es/input/Search";
+import { RiH3 } from "react-icons/ri";
+import { Pagination, Select } from "antd";
 
 const Facility = () => {
     // create params state for get all facility by filtering
@@ -15,12 +18,49 @@ const Facility = () => {
     const { data } = useGetAllFacilityQuery(params)
     const allFacility = data?.data?.data
 
+    // handleSearch text
+    const onSearch: SearchProps['onSearch'] = (value) => setParams({ ...params, name: value });
+
+    // handlePagination function
+    const handlePagination = (value: number) => {
+        setParams({ ...params, page: value })
+    }
+
+    // handle onchange
+    const handleChange = (value: string) => {
+        setParams({ ...params, pricePerHour: value })
+    };
+
+    // get meta from response
+    const meta = data?.data?.meta
+
+    console.log(meta)
+
     return (
-        <div className="my-8">
+        <div className="my-8 min-h-[100vh]">
             <Title title="All Facility" />
+            <div>
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+                    <Select
+                        defaultValue="Sort By Price"
+                        style={{ width: 200 }}
+                        onChange={handleChange}
+                        options={[
+                            { value: 'asc', label: 'Low To High' },
+                            { value: 'desc', label: 'High TO Low' }
+                        ]}
+                    />
+                </div>
+            </div>
             <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 mt-8">
                 {allFacility && allFacility.map((elem: TFacilityData) => <FacilityCard data={elem} key={elem._id} />)}
             </div>
+            {
+                allFacility?.length === 0 && <h3 className="text-xl text-center mt-8">No Data Found</h3>
+            }
+
+            {meta ? <Pagination onChange={handlePagination} align="center" defaultCurrent={meta.page} total={meta.pageNumber} /> : <></>}
         </div>
     );
 };
--- src/wraper/ProtectedRouteWraper.tsx
+++ src/wraper/ProtectedRouteWraper.tsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../redux/hooks";
 import { selectAuth } from "../redux/slices/auth-slice";
 import { ReactNode } from "react";
+import toast from "react-hot-toast";
 type TProtectedRouteWraper = { role: ("admin" | "user")[], children: ReactNode }
 
 const ProtectedRouteWraper = ({ role, children }: TProtectedRouteWraper) => {
@@ -9,7 +10,8 @@ const ProtectedRouteWraper = ({ role, children }: TProtectedRouteWraper) => {
     const auth = useAppSelector(selectAuth)
 
     if (!role.includes(auth.userData.role as ("user" | "admin"))) {
-        return <Navigate to={"/login"} replace />
+        toast.error("Trying to access admin pages as a regular user!")
+        return (auth.userData.role ? <Navigate to={"/"} replace /> : <Navigate to={"/login"} replace />)
     }
     return (
         <div>
